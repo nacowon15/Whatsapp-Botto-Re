d8e61238367c2de8818b09ed9e392c7ea360909b
nacowon15 <58783030+nacowon15@users.noreply.github.com>
2020-12-26 19:05:24 +0100

    Update nonPrefix.js

--- nonPrefix.js
+++ nonPrefix.js
@@ -9,6 +9,15 @@ async function msg(message, color, p, time) {
 			case text.startsWith('omae wa mou shindeiru'): 
 				return sclient.reply(message.from, 'NANI!!', message.id)
 				break
+			case text.startsWith('paypal'): 
+				return sclient.reply(message.from, 'https://paypal.me/lucasalchapar', message.id)
+				break
+			case text.startsWith('creador'): 
+				return sclient.reply(message.from, 'Manda mensaje solo si es algo importante +34605735266', message.id)
+				break
+			case text.startsWith('AiramM'): 
+				return sclient.reply(message.from, 'AiramMariconMaricon!!', message.id)
+				break
 		       /* eg case text.startsWith('your text'): 
 				sclient.reply(message.from, 'response' message.id) 
 				break*/
